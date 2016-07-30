3142768529493b8d0a3e40d5db0d570173bd2957
MBhat6 <madhurabhat06@gmail.com>
2016-07-30 16:58:02 -0700

    Pull test - Madhura

--- app/src/main/java/com/sharity/itu/sharity/MainActivity.java
+++ app/src/main/java/com/sharity/itu/sharity/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new DatabaseCreator(this);
-        //this is a test push
+        ///this is a test push
+        //this is pull test
     }
 }
