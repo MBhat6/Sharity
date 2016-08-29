173e31f3a7fac52a345ea15a0fdd5fb1a7a205f9
MBhat6 <madhurabhat06@gmail.com>
2016-08-29 03:03:41 -0700

    My profile function - Sharity

--- app/src/main/java/com/sharity/itu/sharity/DatabaseCreator.java
+++ app/src/main/java/com/sharity/itu/sharity/DatabaseCreator.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -53,7 +54,6 @@ public class DatabaseCreator extends SQLiteOpenHelper {
     String DateToStr = format.format(curDate);
 
 
-
     public DatabaseCreator(Context context) {
 
         super(context, DATABSE_NAME, null, 1);
@@ -68,15 +68,15 @@ public class DatabaseCreator extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE " + USER_INFO_TABLE + "("+ USER_NAME + " VARCHAR(60) NOT NULL, "
-                                                          + PASSWORD + " VARCHAR(50) NOT NULL, "
-                                                          + EMAIL_ID + " VARCHAR(100) PRIMARY KEY NOT NULL, "
-                                                          + CURRENT_PROG + " VARCHAR(100) NOT NULL, "
-                                                          + INTR_TOPICS + " VARCHAR(200) NOT NULL, "
-                                                          + EXPERTISE + " VARCHAR(200) NOT NULL, "
-                                                          + HINT + " VARCHAR(22) NOT NULL ) ");
+            db.execSQL("CREATE TABLE " + USER_INFO_TABLE + "(" + USER_NAME + " VARCHAR(60) NOT NULL, "
+                                                                + PASSWORD + " VARCHAR(50) NOT NULL, "
+                                                                + EMAIL_ID + " VARCHAR(100) PRIMARY KEY NOT NULL, "
+                                                                + CURRENT_PROG + " VARCHAR(100) NOT NULL, "
+                                                                + INTR_TOPICS + " VARCHAR(200) NOT NULL, "
+                                                                + EXPERTISE + " VARCHAR(200) NOT NULL, "
+                                                                + HINT + " VARCHAR(22) NOT NULL ) ");
 
-        db.execSQL("CREATE TABLE " + REQUEST_TABLE + "("    + REQUEST_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            db.execSQL("CREATE TABLE " + REQUEST_TABLE + "(" + REQUEST_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                                                             + USER_NAME + " VARCHAR(60) NOT NULL, "
                                                             + EMAIL_ID + " VARCHAR(100) NOT NULL, "
                                                             + CATEGORY + " VARCHAR(50) NOT NULL, "
@@ -90,8 +90,6 @@ public class DatabaseCreator extends SQLiteOpenHelper {
     }
 
 
-
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
@@ -102,14 +100,16 @@ public class DatabaseCreator extends SQLiteOpenHelper {
 
     /**
      * Save the request details
+     *
      * @param saveDetails
      */
-    public void saveRequest(Details saveDetails){
+    public void saveRequest(Details saveDetails) {
 
         try {
-            Log.i("Date value:" , DateToStr);
+            Log.i("Date value:", DateToStr);
             Log.i("User name ", saveDetails.getName());
             Log.i("User email ", saveDetails.getEmail());
+            //  Log.i("User Password", saveDetails.getPassword());
 
             SQLiteDatabase db = this.getWritableDatabase();
             ContentValues values = new ContentValues();
@@ -119,35 +119,36 @@ public class DatabaseCreator extends SQLiteOpenHelper {
             values.put(PRIORITY, saveDetails.getPriority());
             values.put(TITLE, saveDetails.getTitle());
             values.put(DESCRIPTION, saveDetails.getDesc());
+            //  values.put(PASSWORD, saveDetails.getPassword());
             values.put(CREATE_DATE, DateToStr);
             values.put(REPLY_USER_NAME, "");
 
             long rowInserted = db.insert(REQUEST_TABLE, COMMENTS, values);
             db.close();
 
-            if(rowInserted != -1) {
+            if (rowInserted != -1) {
 
                 Log.i("Row insertion", "New request row ID: " + rowInserted);
                 Toast.makeText(contextDb, "New request has been created. ", Toast.LENGTH_SHORT).show();
 
-                Log.i("clicks","Navigating to home page");
+                Log.i("clicks", "Navigating to home page");
                 Intent i = new Intent(contextDb, HomePage.class);
 
                 Bundle detailBundle = new Bundle();
 
                 detailBundle.putString("EMAIL", saveDetails.getEmail());
                 detailBundle.putString("NAME", saveDetails.getName());
+                //  detailBundle.putString("PASSWORD",saveDetails.getPassword());
 
                 i.putExtras(detailBundle);
 
                 contextDb.startActivity(i);
-            }
-            else {
+            } else {
                 Toast.makeText(contextDb, "Oops!!! Something went wrong", Toast.LENGTH_SHORT).show();
             }
         } catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(contextDb, "An error occurred while creating a new request. Please try again later",Toast.LENGTH_SHORT).show();
+            Toast.makeText(contextDb, "An error occurred while creating a new request. Please try again later", Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -155,9 +156,10 @@ public class DatabaseCreator extends SQLiteOpenHelper {
 
     /**
      * To sign up a new user
+     *
      * @param details
      */
-    public void insertContact(Details details){
+    public void insertContact(Details details) {
 
         try {
 
@@ -168,13 +170,13 @@ public class DatabaseCreator extends SQLiteOpenHelper {
             ContentValues values = new ContentValues();
 
             String query = "SELECT * FROM  " + USER_INFO_TABLE + " WHERE EMAIL_ID =  '" + details.getEmail() + "'";
-            Log.i("query: " , query);
+            Log.i("query: ", query);
             Cursor cursor = db.rawQuery(query, null);
             int count = cursor.getCount();
 
             if (count == 0) {
 
-                Log.i("values : " , details.getEmail());
+                Log.i("values : ", details.getEmail());
 
                 values.put(USER_NAME, details.getName());
                 values.put(PASSWORD, details.getPassword());
@@ -188,26 +190,24 @@ public class DatabaseCreator extends SQLiteOpenHelper {
 
                 db.close();
 
-                if(rowId != -1) {
+                if (rowId != -1) {
 
                     Log.i("Sign up row insertion", "New sign up row ID: " + rowId);
                     Toast.makeText(contextDb, "You are successfully registered. ", Toast.LENGTH_SHORT).show();
 
-                    Log.i("clicks","Navigating to login page");
+                    Log.i("clicks", "Navigating to login page");
                     Intent i = new Intent(contextDb, MainActivity.class);
                     contextDb.startActivity(i);
-                }
-                else {
+                } else {
                     Toast.makeText(contextDb, "Sign up was not successful. Please try again", Toast.LENGTH_SHORT).show();
                 }
 
-            }
-            else {
+            } else {
                 Toast.makeText(contextDb, "You are already a member. Please login to the app", Toast.LENGTH_SHORT).show();
             }
         } catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(contextDb, "An error occurred while signing in up for the app. Please try again",Toast.LENGTH_SHORT).show();
+            Toast.makeText(contextDb, "An error occurred while signing in up for the app. Please try again", Toast.LENGTH_SHORT).show();
         }
 
 
@@ -215,6 +215,7 @@ public class DatabaseCreator extends SQLiteOpenHelper {
 
     /**
      * User login authentication
+     *
      * @param email
      * @return
      */
@@ -232,28 +233,27 @@ public class DatabaseCreator extends SQLiteOpenHelper {
         detailsList.add(dbPwd);
         detailsList.add(dbName);
 
-        try{
+        try {
             SQLiteDatabase db = this.getReadableDatabase();
 
-            String query = "SELECT EMAIL_ID, PASSWORD, USER_NAME FROM  "+USER_INFO_TABLE + " WHERE EMAIL_ID = '" + email + "'";
+            String query = "SELECT EMAIL_ID, PASSWORD, USER_NAME FROM  " + USER_INFO_TABLE + " WHERE EMAIL_ID = '" + email + "'";
 
-            Log.i("Sign in query: " , query);
+            Log.i("Sign in query: ", query);
 
             Cursor cursor = db.rawQuery(query, null);
             int check = cursor.getCount();
 
-            Log.i(" counter " , String.valueOf(check));
+            Log.i(" counter ", String.valueOf(check));
 
-            if(cursor.moveToFirst()) {
-                do{
+            if (cursor.moveToFirst()) {
+                do {
 
                     detailsList.clear();
                     dbEmail = cursor.getString(0);
 
                     detailsList.add(cursor.getString(0));
 
-                    if(dbEmail.equals(email))
-                    {
+                    if (dbEmail.equals(email)) {
                         dbPwd = cursor.getString(1);
                         dbName = cursor.getString(2);
 
@@ -263,16 +263,16 @@ public class DatabaseCreator extends SQLiteOpenHelper {
                         break;
                     }
                 }
-                while(cursor.moveToNext());
+                while (cursor.moveToNext());
             }
-            if(check == 0){
+            if (check == 0) {
                 Toast.makeText(contextDb, "You are not a member yet. Please Sign Up", Toast.LENGTH_SHORT).show();
             }
 
         } catch (Exception e) {
 
             e.printStackTrace();
-            Toast.makeText(contextDb, "An error occurred while logging into for the app. Please try again",Toast.LENGTH_SHORT).show();
+            Toast.makeText(contextDb, "An error occurred while logging into for the app. Please try again", Toast.LENGTH_SHORT).show();
         }
         return detailsList;
     }
@@ -280,6 +280,7 @@ public class DatabaseCreator extends SQLiteOpenHelper {
 
     /**
      * Forgot password - fetch hint
+     *
      * @param hint
      * @return
      */
@@ -289,7 +290,7 @@ public class DatabaseCreator extends SQLiteOpenHelper {
 
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor c = db.rawQuery("SELECT * FROM " + USER_INFO_TABLE + " WHERE HINT = '" + hint + "' AND EMAIL_ID = '" + email + "'" , null);
+        Cursor c = db.rawQuery("SELECT * FROM " + USER_INFO_TABLE + " WHERE HINT = '" + hint + "' AND EMAIL_ID = '" + email + "'", null);
 
         if (c.moveToFirst()) {
             do {
@@ -301,12 +302,11 @@ public class DatabaseCreator extends SQLiteOpenHelper {
 
     /**
      * Get profile details
+     *
      * @param name
      * @return
      */
     public ArrayList getProfileDetails(String name, String email) {
-
-        String str = null;
         ArrayList list = new ArrayList();
 
         SQLiteDatabase db = this.getReadableDatabase();
@@ -324,4 +324,95 @@ public class DatabaseCreator extends SQLiteOpenHelper {
         return list;
     }
 
+    /**
+     * Saving the profile details
+     * @param saveObj
+     */
+    public void change(Details saveObj) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Log.i("----Profile: NewPw -Old", saveObj.getPassword());
+        Log.i("---Profile: Hint -Old", saveObj.getPassword());
+
+        try {
+            if(saveObj.getPassword().length() == 0 || saveObj.getPassword() == "") {
+
+                Cursor c = db.rawQuery("SELECT " + PASSWORD + "  FROM  " + USER_INFO_TABLE + " WHERE EMAIL_ID  = '" + saveObj.getEmail() + "'", null);
+
+                if (c.moveToFirst()) {
+                    do {
+                        saveObj.setPassword(c.getString(c.getColumnIndex("PASSWORD")));
+                        Log.i("--------NEWPw------", saveObj.getPassword());
+                    } while (c.moveToNext());
+                }
+            }
+            Cursor hintCursor = db.rawQuery("SELECT " + HINT + "  FROM  " + USER_INFO_TABLE + " WHERE EMAIL_ID  = '" + saveObj.getEmail() + "'", null);
+
+            if(hintCursor.moveToFirst()) {
+                do {
+                    saveObj.setHint(hintCursor.getString(hintCursor.getColumnIndex("HINT")));
+                    Log.i("--------Hint------", saveObj.getPassword());
+
+                } while (hintCursor.moveToNext());
+            }
+            ContentValues values = new ContentValues();
+            values.put(USER_NAME, saveObj.getName());
+            values.put(EMAIL_ID, saveObj.getEmail());
+            values.put(PASSWORD, saveObj.getPassword());
+            values.put(CURRENT_PROG, saveObj.getCourse());
+            values.put(EXPERTISE, saveObj.getExpertise());
+            values.put(INTR_TOPICS, saveObj.getInterest());
+            values.put(HINT, saveObj.getHint());
+
+            long rowInserted = db.update(USER_INFO_TABLE, values, " EMAIL_ID =  '"+ saveObj.getEmail() +"' ", null);
+
+            db.close();
+
+            if(rowInserted != -1){
+                Log.i("clicks", "Success in updating Profile");
+
+                Toast.makeText(contextDb,"Saved successfully", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(contextDb, HomePage.class);
+
+                Bundle detailBundle = new Bundle();
+
+                detailBundle.putString("EMAIL", saveObj.getEmail());
+                detailBundle.putString("NAME", saveObj.getName());
+
+                i.putExtras(detailBundle);
+
+                contextDb.startActivity(i);
+            }
+            else {
+                Log.i("clicks", "Error in updating the Profile");
+                Toast.makeText(contextDb,"Profile update was not successful", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(contextDb, "Error saving your Profile settings. Please try again", Toast.LENGTH_SHORT);
+
+        }
+    }
+
+    public List fetchRows(){
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        List highList = null;
+
+        Cursor row = db.rawQuery("SELECT  REQUEST_ID, TITLE, USER_NAME,  CATEGORY,  DESCRIPTION, " + CREATE_DATE + " FROM  " + REQUEST_TABLE + " WHERE  PRIORITY = 'High' ", null);
+
+        if (row.moveToFirst()) {
+            do {
+
+
+
+
+
+            } while (row.moveToNext());
+        }
+        return highList;
+    }
 }
